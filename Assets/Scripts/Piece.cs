4553122a03281cd0037876a68d128ae850261558
Michael Dorst <mikefdorst@mikefdorst.com>
2018-05-06 21:14:05 -0700

    Assign starting tiles to pieces

---
+++ Assets/Scripts/Piece.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Piece : MonoBehaviour
+{
+    // Set in editor
+    public Tile StartingTile;
+
+    private Tile _currentTile;
+
+    private void Start()
+    {
+        _currentTile = StartingTile;
+    }
+}
--- Assets/Scripts/Tile.cs
+++ Assets/Scripts/Tile.cs
@@ -1,7 +1,17 @@
 ﻿using UnityEngine;
 
-public class Tile : MonoBehaviour {
-    
-    
-    
+public class Tile : MonoBehaviour
+{
+    private int _rank;
+    private int _file;
+    public int RankInt => _rank;
+    public int FileInt => _file;
+    public char RankChar => (char) ((int) '1' + _rank);
+    public char FileChar => (char) ((int) 'A' + _file);
+
+    private void Start()
+    {
+        _rank = (int) name[0] - '1';
+        _file = (int) name[1] - 'A';
+    }
 }
